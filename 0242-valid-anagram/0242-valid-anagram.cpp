179f7223f8413174171b30a02b84b6089163dea2
RockCoder <145030135+RockCoder108@users.noreply.github.com>
2025-06-04 08:48:27 +0530

    Time: 1 ms (62.23%), Space: 10 MB (11.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+
+        unordered_map<char,int>m1;
+        unordered_map<char,int>m2;
+        for(const auto  &el:s) {
+            m1[el]++;
+        }
+        for(const auto  &el:t) {
+            m2[el]++;
+        }
+        return m1==m2;
+
     }
 };
