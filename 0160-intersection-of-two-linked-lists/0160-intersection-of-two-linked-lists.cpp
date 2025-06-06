f5dd1a918c9cd6648c092d4749a8cf8033e859a9
RockCoder <145030135+RockCoder108@users.noreply.github.com>
2025-06-06 10:35:51 +0530

    Time: 35 ms (70.54%), Space: 18.5 MB (81.59%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,37 +8,83 @@
  */
 class Solution {
 public:
-    int getlength(ListNode *head){
-        int count=1;
-        ListNode* curr=head;
-        while(curr!=NULL){
-            count++;
-            curr=curr->next;
-        }
-        return count;
-    }
+    // int getlength(ListNode *head){
+    //     int count=1;
+    //     ListNode* curr=head;
+    //     while(curr!=NULL){
+    //         count++;
+    //         curr=curr->next;
+    //     }
+    //     return count;
+    // }
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        int n = getlength(headA);
-        int m = getlength(headB);
+        // int n;
+        // int count=1;
+        // ListNode* curr=headA;
+        // while(curr!=NULL){
+        //     count++;
+        //     curr=curr->next;
+        // }
+        // n=count;
+        // int m;
+        // int counts=1;
+        // ListNode* currs=headB;
+        // while(currs!=NULL){
+        //     counts++;
+        //     currs=currs->next;
+        // }
+        // m=counts;
+        
+        // while(n > m){
+        //     n--;
+        //     headA=headA->next;
+        // }
 
-        while(n > m){
-            n--;
+        // while(n < m){
+        //     m--;
+        //     headB=headB->next;
+        // }
+        // ListNode* temp;
+        // while(headA!=NULL){
+        //     if(headA==headB){
+        //         temp=headA;
+        //         break;
+        //     }
+        //     headA=headA->next;
+        //     headB=headB->next;
+        // }
+        // return temp;
+
+
+
+        int count1=0;
+        int count2=0;
+        ListNode *currA=headA;
+        ListNode *currB=headB;
+        while(currA!=NULL){
+            count1++;
+            currA=currA->next;
+        }
+        while(currB!=NULL){
+            count2++;
+            currB=currB->next;
+        }
+        while(count1 > count2){
+            count1--;
             headA=headA->next;
         }
-
-        while(n < m){
-            m--;
+        while(count1 < count2){
+            count2--;
             headB=headB->next;
         }
-        ListNode* temp;
+        ListNode * temp;
         while(headA!=NULL){
             if(headA==headB){
-                temp=headA;
-                break;
+                return headA;
             }
             headA=headA->next;
             headB=headB->next;
         }
-        return temp;
+        return nullptr;
     }
 };
