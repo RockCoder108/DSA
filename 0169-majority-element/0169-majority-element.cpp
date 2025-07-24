d536b913f43ec0fdb31e32639731bafd7867538f
RockCoder <145030135+RockCoder108@users.noreply.github.com>
2025-07-24 11:47:51 +0530

    Time: 0 ms (100%), Space: 28.1 MB (86.3%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        int element=nums[0], count=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=element){
+    int majorityElement(vector<int>& arr) {
+        // int element=nums[0], count=1;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i]!=element){
+        //         count--;
+        //         if(count==0){
+        //             element=nums[i];
+        //             count++;
+        //         }
+        //     }else{
+        //         count++;
+        //     }
+        // }
+        // return element;
+
+        int n =arr.size();
+        int num = arr[0];
+        int count=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]==num){
+                count++;
+            }else{
                 count--;
                 if(count==0){
-                    element=nums[i];
+                    num = arr[i];
                     count++;
                 }
-            }else{
-                count++;
             }
         }
-        return element;
+        return num;
     }
 };
+
